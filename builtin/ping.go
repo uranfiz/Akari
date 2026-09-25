f205688cef96bc3f0df934fcc6185e3c834692f9
uranfiz <devdreamix@gmail.com>
2026-09-25 16:49:44 +0300

    Update ping.go

--- builtin/ping.go
+++ builtin/ping.go
@@ -4,10 +4,8 @@ import (
 	"fmt"
 	"log"
 	"time"
-
 	"akari/api"
 	"akari/core"
-
 	"github.com/mtgo-labs/mtgo/telegram"
 )
 
@@ -27,30 +25,32 @@ func init() {
 }
 
 func cmdPing(ctx *telegram.Context) error {
-	start := time.Now()
-
 	if ctx.Message == nil {
 		return nil
 	}
 
 	chatID := ctx.Message.ChatID
 	msgID := ctx.Message.ID
-
 	if chatID == 0 || msgID == 0 {
 		return nil
 	}
 
 	api.CachePeer(ctx, chatID)
+	start := time.Now()
 
+	_, err := ctx.Client.EditMessageText(ctx.Ctx, chatID, msgID, "🏓")
+	if err != nil {
+		log.Printf("ping placeholder edit error: %v", err)
+		return err
+	}
 	pingMs := float64(time.Since(start).Microseconds()) / 1000.0
-
 	text := fmt.Sprintf(
 		"🏓 Ping: %.3f ms\n⏱ Uptime: %s",
 		pingMs,
 		api.FormatUptime(time.Since(startTime)),
 	)
 
-	_, err := ctx.Client.EditMessageText(ctx.Ctx, chatID, msgID, text)
+	_, err = ctx.Client.EditMessageText(ctx.Ctx, chatID, msgID, text)
 	if err != nil {
 		log.Printf("ping edit error: %v", err)
 	}
